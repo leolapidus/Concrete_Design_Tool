105836bbdadad6633a6f549ecf5983907fc1a931
Leo Lapidus <lapidus.leo@gmail.com>
2019-06-24 18:04:31 +0200

    add hyperjet

--- Concrete_Design/bending_without_n.py
+++ Concrete_Design/bending_without_n.py
@@ -7,6 +7,7 @@ import numpy as np
 from FE_code.model import Model
 from FE_code.beam_column_element import BeamColumnElement
 from Concrete_Design.values import Values 
+from Concrete_Design.debug_print import debug
 
 
 #TODO: Unterscheiden zwischen Feldbereich und Stützbereich 
@@ -28,8 +29,9 @@ def bending_without_n(model, values, concrete_type, exp):
     """
     m = []
     erf_As = []
+    diff_erf_As = []
     fcd = values.concrete(concrete_type)['fcd']
-    for ele in model.elements:
+    for i, ele in enumerate(model.elements):
         if type(ele)==BeamColumnElement:
             m.append(ele.local_internal_forces[2])
             m.append(ele.local_internal_forces[5]*-1)
@@ -37,18 +39,27 @@ def bending_without_n(model, values, concrete_type, exp):
             m_ed = max(abs(m[0]), abs(m[1]))
             m_ed = m_ed*0.001
 
-            b = ele.b
-            h = ele.h 
+            diff_m_ed = m_ed.g
+
+            #debug('m_ed')
+
             _calculate_concrete_cover = values.concrete_cover(exp)
             
-            mue_eds = m_ed/(b*(values.static_usable_height(h)**2)*fcd)
+            mue_eds = m_ed/(ele.b*(values.static_usable_height(ele.h)**2)*fcd)
+
+            #debug('mue_eds')
 
             _table=values.design_table_values()
 
             omega = values.interpolate_omega(mue_eds)
             sigma = values.interpolate_sigma(mue_eds)
 
-            erf_As.append(1/sigma*(omega*b*values.static_usable_height(h)*fcd)*10000) # cm²
+            As = 1/sigma*(omega*ele.b*values.static_usable_height(ele.h)*fcd)*10000
+
+            erf_As.append(1/sigma*(omega*ele.b*values.static_usable_height(ele.h)*fcd)*10000) # cm²
+
+            diff_erf_As.append(erf_As[i].g)
+            #debug('As')
 
             del m[0]
             del m[0]
